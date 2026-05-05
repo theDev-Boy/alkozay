f42093201aaccd332024447ecc9491f7151103f8
MuhammadAfnan02 <mustafa.afnan01@gmail.com>
2026-05-05 05:36:10 +0500

    Fixed again

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,17 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            if (android != null && android.namespace == null) {
+                android.namespace = "dev.flutter.plugins.${project.name.replace("-", "_")}"
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
